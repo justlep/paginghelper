e1d7ab30bf3281277735f1344049046ed1845577
Lennart Pegel <justlep@users.noreply.github.com>
2016-10-11 13:36:06 +0200

    Update PagingHelper.js

--- src/PagingHelper.js
+++ src/PagingHelper.js
@@ -6,7 +6,7 @@
  *
  * @license MIT
  */
-;(function (factory) {
+;(function (root, factory) {
     'use strict';
 
     if (typeof define === 'function' && define.amd) {
@@ -14,9 +14,9 @@
     } else if (typeof module === 'object' && typeof (module||{}).exports === 'object') {
         module.exports = factory();
     } else {
-        window.PagingHelper = factory();
+        root.PagingHelper = factory();
     }
-})(function() {
+})(this, function() {
 
     'use strict';
 
